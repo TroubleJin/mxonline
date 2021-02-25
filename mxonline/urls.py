3749f5c1f7d9586c99a7ac357369014c3ddde48c
Administrator <admin@example.com>
2021-02-25 10:08:35 +0800

    fix

--- mxonline/urls.py
+++ mxonline/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 import xadmin
+
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
+    path('',  TemplateView.as_view(template_name='index.html')),
+    path('login/',  TemplateView.as_view(template_name='login.html')),
 ]
